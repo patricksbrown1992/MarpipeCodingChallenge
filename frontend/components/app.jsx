e450856c241beaebf350a68108784c261e4f4f77
patrickbrown <patricksbrown1992@gmail.com>
2019-09-24 22:00:19 -0400

    working skeleton

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Splash from '../components/splash/splashForm'
+
+const App = () => (
+    <div>
+        <h1>Marpipe</h1>
+        <Splash />
+    </div>
+);
+
+export default App;
---
+++ frontend/reducers/rootReducer.jsx
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities/entitiesReducer';
+import sessionReducer from './sessionReducer';
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+
+});
+
+export default rootReducer;
