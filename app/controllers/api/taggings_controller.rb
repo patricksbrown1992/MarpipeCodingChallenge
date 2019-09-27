deb0223b10c91f17e64bfcdea3328aa3003364bd
patrickbrown <patricksbrown1992@gmail.com>
2019-09-27 09:30:26 -0400

    controllers

---
+++ app/controllers/api/taggings_controller.rb
@@ -0,0 +1,51 @@
+class Api::TaggingsController < ApplicationController
+    # before_action :require_logged_in
+
+    def index
+        @user = User.find(params[:user_id])
+        @taggings = @user.taggings
+        render json: @taggings, status: 200
+    end
+
+    def show
+        @tagging = Tagging.find(params[:id])
+        if @tagging
+            render json: @tagging, status: 200
+        else
+            render json: @tagging.errors.full_messages, status: 404
+        end
+    end
+
+    def create 
+        @tagging = Tagging.new(tagging_params)
+        if @tagging.save
+            render json: @tagging, status: 200
+        else
+            render json: @tagging.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @tagging = Tagging.find(params[:id])
+        if @tagging.destroy
+            render json: @tagging, status: 200
+        else
+            render json: @tagging.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+         @tagging = Tagging.find(params[:id])
+        if @tagging.update(tagging_params)
+            render json: @tagging, status: 200
+        else
+            render json: @tagging.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def tagging_params
+        params.require(:tagging).permit(:post_id, :tag_id, :user_id)
+    end
+end
